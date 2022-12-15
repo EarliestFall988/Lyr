39d60461c96d2bb80c72c4970dc190b91505e9e9
Taylor Howell <howelltaylor195@gmail.com>
2022-12-14 22:42:22 -0600

    fixing references

--- pages/About.vue
+++ pages/About.vue
@@ -1,15 +1,19 @@
 <template>
-  <div class="text-white ">
+  <div class="text-white">
     <PageHeader title="About" />
+    <p class="text-gray-400">
+      Music Was Found on
+      <a class="bg-blue-800 rounded p-1 text-white" href="https://genius.com"
+        >Genius.com</a
+      >
+    </p>
     <p class="mt-5 text-gray-400">This website was created by Taylor Howell</p>
-    <p class="mt-5 text-gray-400">If you have any questions or comments message me at howelltaylor747@gmail.com</p>
+    <p class="mt-5 text-gray-400">
+      If you have any questions or comments message me at howelltaylor747@gmail.com
+    </p>
   </div>
 </template>
 
 <script>
-export default {
-  
-};
+export default {};
 </script>
-
-
