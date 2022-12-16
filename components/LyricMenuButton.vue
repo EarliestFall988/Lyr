e8bc9ba97fc6d398824349b400e16fa8ec7ebad0
Taylor Howell <howelltaylor195@gmail.com>
2022-12-15 18:25:57 -0600

    tweaking UI

--- components/LyricMenuButton.vue
+++ components/LyricMenuButton.vue
@@ -1,15 +1,16 @@
 <template>
     <div>
-        <button class="text-lg text-gray-200 bg-gray-900 p-2 rounded" @click="toggleLyrics">{{ title }}</button>
-        <p class="text-sm text-gray-500 mx-2 mb-4">{{ credits }}</p>
-        <Transition enter-from-class="translate-y-[-5%] opacity-0" enter-active-class="transition duration-30"
-            leave-to-class="translate-y-[-5%] opacity-0" leave-active-class="transition duration-30">
+        <button class="text-lg text-gray-200 bg-gray-900 p-2 rounded-t w-[100%]" @click="toggleLyrics">{{ title }}</button>
+       
+        <Transition enter-from-class="translate-y-[-5%] opacity-0" enter-active-class="transition duration-300"
+            leave-to-class="translate-y-[-5%] opacity-0" leave-active-class="transition duration-300">
         <div v-if="lyricsOpen">
-            <p class="overflow-x-hidden text-sm bg-gray-900 rounded p-1 my-3 border-[1px] border-gray-600" >
+            <p class="overflow-x-hidden text-sm bg-gray-900 rounded-b p-2 border-t-2 border-gray-600 leading-loose" >
             <pre>{{ lyrics }}</pre>
             </p>
         </div>
         </Transition>
+        <p class="text-sm text-gray-500 mx-2 mb-4">{{ credits }}</p>
     </div>
 </template>
 
